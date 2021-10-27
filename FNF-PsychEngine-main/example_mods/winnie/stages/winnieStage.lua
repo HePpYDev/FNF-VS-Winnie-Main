8d37e33280fed7e626568fa71abda998db006d4e
HePpYDev <93030242+HePpYDev@users.noreply.github.com>
2021-10-27 16:57:02 +0200

    Hi again

---
+++ FNF-PsychEngine-main/example_mods/winnie/stages/winnieStage.lua
@@ -0,0 +1,15 @@
+function onCreate()
+
+	makeLuaSprite('Sky','Sky',-725,-160)
+	addLuaSprite('Sky',false)
+
+	makeLuaSprite('Grass','Grass',-725,-160)
+	addLuaSprite('Grass',false)
+
+	makeLuaSprite('Ground','Ground',-725,-160)
+	addLuaSprite('Ground',false)
+
+	makeLuaSprite('Tree','Tree',-725,-160)
+	addLuaSprite('Tree',false)
+
+end	
